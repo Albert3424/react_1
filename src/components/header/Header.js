4f7699072241d29fe57aa874c1ebd931f5f63b72
Albert <Izuku@DESKTOP-125A3DF>
2025-08-31 18:52:52 +0500

    DB Jason 2

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,12 @@
-import logo from '../../logo.svg'
+import logo from '../../logo.svg'   //Scalable Vector Graphics
 
 function Header() {
     let title = "Introduction to React"
-    return
-    (
+    return(
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <h1>{title}</h1>
+            <h2>JSON - JavaScript Object Notation</h2>
             <p>
                 Edit <code>src/App.js</code> and save to reload.
             </p>
