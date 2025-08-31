7827dd8c4cb9f707553ea4eb2aa398ad753cbc8b
Albert <Izuku@DESKTOP-125A3DF>
2025-08-31 18:28:31 +0500

    DB Jason

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 let nav = {"Главная":"/index", "Новости":"/new", "О компании": "/about", "Магазин": "/shop", "Контакты":"/contacts"};
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <App navigation = {nav} />
+    <App navigation = {nav} />
   </React.StrictMode>
 );
 
